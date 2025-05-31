4ccb59a0337dc242d91b03f546a5d16748140955
rocky <rby@dusytfeet.com>
2025-05-31 10:03:23 -0400

    Start 3.11+ seperatation as master

---
+++ admin-tools/setup-python-3.6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Check out python-3.6-to-3.10 and dependent development branches.
+
+bs=${BASH_SOURCE[0]}
+if [[ $0 == $bs ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
+
+PYTHON_VERSION=3.6
+
+xpython_owd=$(pwd)
+mydir=$(dirname $bs)
+fulldir=$(readlink -f $mydir)
+cd $mydir
+. ./checkout_common.sh
+(cd $fulldir/.. && checkout_version x-python python-3.6)
+checkout_finish python-3.6-to-3.10
