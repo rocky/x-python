3435ee8676f6390f42442ecb4a9a8af9e547f515
rocky <rby@dusytfeet.com>
2025-06-01 20:12:06 -0400

    Administrivia
    Add checking for 3.6-3.10 versions and latest version

---
+++ admin-tools/check-3.6-3.10-versions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+function finish {
+  cd $owd
+}
+
+# FIXME put some of the below in a common routine
+owd=$(pwd)
+trap finish EXIT
+
+cd $(dirname ${BASH_SOURCE[0]})
+
+if ! source ./pyenv-3.6-3.10-versions ; then
+    exit $?
+fi
+cd ..
+[[ -f test/.python-version ]] && rm -v test/.python-version
+(cd ../python-xdis/admin-tools && source ./setup-python-3.6.sh)
+git checkout python-3.6
+
+for version in $PYVERSIONS; do
+    echo --- $version ---
+    if ! pyenv local $version ; then
+	exit $?
+    fi
+    make clean && pip install -e .
+    if ! make check; then
+	exit $?
+    fi
+    echo === $version ===
+done
