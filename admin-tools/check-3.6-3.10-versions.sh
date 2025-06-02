ffcc039a9e6c34c38a98b99ad7e1300c8bd6a232
rocky <rby@dusytfeet.com>
2025-06-01 20:17:17 -0400

    Merge hell

--- admin-tools/check-3.6-3.10-versions.sh
+++ admin-tools/check-3.6-3.10-versions.sh
@@ -10,22 +10,13 @@ trap finish EXIT
 
 cd $(dirname ${BASH_SOURCE[0]})
 
-<<<<<<< HEAD
-if ! source ./pyenv-3.3-3.5-versions ; then
-=======
 if ! source ./pyenv-3.6-3.10-versions ; then
->>>>>>> master
     exit $?
 fi
 cd ..
 [[ -f test/.python-version ]] && rm -v test/.python-version
-<<<<<<< HEAD
-(cd ../python-xdis/admin-tools && source ./setup-python-3.3.sh)
-git checkout python-3.3
-=======
 (cd ../python-xdis/admin-tools && source ./setup-python-3.6.sh)
-git checkout python-3.6
->>>>>>> master
+git checkout python-3.6-to-3.10
 
 for version in $PYVERSIONS; do
     echo --- $version ---
