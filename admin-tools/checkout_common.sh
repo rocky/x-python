2fcf5046357f9638c79a184481f21a6318545310
rocky <rby@dusytfeet.com>
2025-10-10 02:21:01 -0400

    Bump version & administrivia

--- admin-tools/checkout_common.sh
+++ admin-tools/checkout_common.sh
@@ -2,20 +2,20 @@
 export PATH=$HOME/.pyenv/bin/pyenv:$PATH
 bs=${BASH_SOURCE[0]}
 mydir=$(dirname $bs)
-fulldir=$(readlink -f $mydir)
+x_python_fulldir=$(readlink -f $mydir)
 
 function setup_version {
     local repo=$1
     version=$2
     echo Running setup $version on $repo ...
-    (cd ../$repo && . ./admin-tools/setup-${version}.sh)
+    (cd $x_python_fulldir/../$repo && . ./admin-tools/setup-${version}.sh)
     return $?
 }
 
 function checkout_finish {
     branch=$1
-    cd $mydir/.. && pyenv local $PYTHON_VERSION
-    git checkout $branch && git pull
+    cd $x_python_fulldir/..
+    git checkout $branch && pyenv local $PYTHON_VERSION && git pull
     cd $xpython_owd
     rc=$?
     return $rc
--- admin-tools/setup-master.sh
+++ admin-tools/setup-master.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # Check out master branch and dependent development master branches
+PYTHON_VERSION=3.11
+
+xpython_owd=$(pwd)
 bs=${BASH_SOURCE[0]}
 if [[ $0 == $bs ]] ; then
     echo "This script should be *sourced* rather than run directly through bash"
     exit 1
 fi
 
-PYTHON_VERSION=3.11
-
 xpython_owd=$(pwd)
 mydir=$(dirname $bs)
-fulldir=$(readlink -f $mydir)
-cd $mydir
+x_python_fulldir=$(readlink -f $mydir)
 
-if ! source ../admin-tools/pyenv-newest-versions ; then
+if ! source $x_python_fulldir/../admin-tools/pyenv-newest-versions ; then
     exit $?
 fi
 
+. $x_python_fulldir/checkout_common.sh
 
-. ./checkout_common.sh
-(cd $fulldir/.. && setup_version python-xdis master)
+(cd $x_python_fulldir/../../../rocky && setup_version python-xdis master)
 checkout_finish master
